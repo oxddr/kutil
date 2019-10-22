13737e741cc542bf503496d6260afb1ed583f9a1
Janek Łukaszewicz <janluk@google.com>
2019-10-22 11:28:51 +0200

    script: update download-logs.sh

--- scripts/download-logs.sh
+++ scripts/download-logs.sh
@@ -9,22 +9,14 @@ if [ "$artifacts" == "master" ]; then
     artifacts="gce-scale-cluster-master"
 fi
 
-case $run_name in
-    *gke*)
-        bucket=gs://gke-scalability-prow
-        ;;
-    *gce*)
-        bucket=gs://kubernetes-jenkins
-        ;;
-esac
-
-
 output="$HOME/debug/${run_name}"
-
 mkdir -p "${output}"
+
+prefix="gs://kubernetes-jenkins/logs/${run_name}"
+
 if [ -n "$artifacts" ]; then
-    gsutil -m  cp -R "${bucket}"/logs/"${run_name}"/artifacts/"${artifacts}" "${output}"
+    gsutil -m  cp -R "${prefix}/artifacts/${artifacts}" "${output}"
 else
-    gsutil -m  cp "${bucket}"/logs/"${run_name}"/build-log.txt "${output}"
+    gsutil -m  cp "${prefix}/build-log.txt" "${output}"
 fi
 echo "Downloaded to ${output}"
